be4bdf7c9f2a73529854843b3c03f836fc456283
raaz1212 <araj30728@gmail.com>
2023-05-20 05:08:07 +0600

    delete method

--- index.js
+++ index.js
@@ -50,10 +50,24 @@ async function run() {
       res.send(jobs);
     });
 
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/toys", async (req, res) => {
       const newToys = req.body;
       console.log(newToys);
       const result = await toysCollection.insertOne(newToys);
+      res.send(result);
+    });
+
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
 
